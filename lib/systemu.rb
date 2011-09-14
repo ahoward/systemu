eb2058b8f98f3fe00e6feef8dce73214cb557cf9
ara.t.howard <ara.t.howard@gmail.com>
2011-09-13 20:54:31 -0600

    checkpoint.

--- lib/systemu.rb
+++ lib/systemu.rb
@@ -13,7 +13,7 @@ class SystemUniversal
 #
 # constants
 #
-  SystemUniversal::VERSION = '2.3.1' unless SystemUniversal.send(:const_defined?, :VERSION)
+  SystemUniversal::VERSION = '2.4.0' unless SystemUniversal.send(:const_defined?, :VERSION)
   def SystemUniversal.version() SystemUniversal::VERSION end
   def version() SystemUniversal::VERSION end
 #
