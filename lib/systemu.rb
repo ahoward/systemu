61df6abccb2f66e35814fbb7da50910589b76efa
ara.t.howard <ara.t.howard@gmail.com>
2012-03-12 21:03:30 -0600

    v2.5.0

--- lib/systemu.rb
+++ lib/systemu.rb
@@ -14,7 +14,7 @@ class SystemUniversal
 #
 # constants
 #
-  SystemUniversal::VERSION = '2.4.2' unless SystemUniversal.send(:const_defined?, :VERSION)
+  SystemUniversal::VERSION = '2.5.0' unless SystemUniversal.send(:const_defined?, :VERSION)
   def SystemUniversal.version() SystemUniversal::VERSION end
   def version() SystemUniversal::VERSION end
 #
